21b3274d8950a8106589992d26aac4b90e66721a
Rahul Pashikanti <165003726+rahulpashikanti@users.noreply.github.com>
2026-08-21 10:09:51 +0530

    Update baud_rate_generator_tb.v
    Refactor baud rate generator testbench for clarity and efficiency.

--- baud_rate_generator_tb.v
+++ baud_rate_generator_tb.v
@@ -1,86 +1,57 @@
-module baud_rate_tb();
+`timescale 1ns/1ns
+module baudrate_generator_tb();
 
-reg pclk,preset_n;
-reg [1:0] spi_mode_i;
-reg spiswai_i;
+reg pclk,presetn,spiswai_i,cpol_i,cpha_i,ss_i;
+reg [1:0]spi_mode_i;
 reg [2:0] sppr_i,spr_i;
-reg cpol_i,cpha_i,ss_i;
+wire sclk_o,miso_receive_sclk_o,miso_receive_sclk0_o,mosi_send_sclk,mosi_send_sclk0_o;
+wire [11:0] baud_rate_divisor_o;
 
-wire sclk_o,miso_pos,miso_neg,mosi_pos,mosi_neg;
-wire [11:0] baud_rate_divisor;
-
-baud_rate dut(
-    pclk,preset_n,
-    spi_mode_i,spiswai_i,
-    sppr_i,spr_i,
-    cpol_i,cpha_i,ss_i,
-    sclk_o,
-    miso_pos,miso_neg,mosi_pos,mosi_neg,
-    baud_rate_divisor
-);
+parameter t=40;
+baudrate_generator DUT ( pclk,presetn,spi_mode_i,spiswai_i,sppr_i,spr_i,cpol_i,cpha_i,ss_i,sclk_o,miso_receive_sclk_o,miso_receive_sclk0_o,mosi_send_sclk,mosi_send_sclk0_o,baud_rate_divisor_o);
 
 initial
 begin
-    pclk=1'b0;
-    forever #10 pclk=~pclk;
+	pclk=1'b0;
+	forever #(t/2) pclk=~pclk;
 end
 
-task reset;
+task resetn();
 begin
-    @(negedge pclk)
-    preset_n=1'b0;
-    @(negedge pclk)
-    preset_n=1'b1;
+	@(negedge pclk)
+	presetn=1'b0;
+	@(negedge pclk)
+	presetn=1'b1;
 end
 endtask
 
-task initialize;
-begin
-    pclk=1'b0;
-    preset_n=1'b0;
-    spi_mode_i=2'b00;
-    sppr_i=3'b000;
-    spr_i=3'b000;
-    spiswai_i=1'b0;
-    cpol_i=1'b1;
-    cpha_i=1'b0;
-    ss_i=1'b0;
-end
+task fixed_stimulus(input spiswai_i1,ss_i1,input [1:0]spi_mode_i1);
+	begin
+		spiswai_i=spiswai_i1;
+		ss_i=ss_i1;
+		spi_mode_i=spi_mode_i1;
+	end
 endtask
 
-task stimulus(input [2:0] i,input [2:0] j,input x,input y);
-begin
-    spi_mode_i=2'b01;
-    sppr_i=i;
-    spr_i=j;
-    spiswai_i=1'b0;
-    cpol_i=x;
-    cpha_i=y;
-    ss_i=1'b0;
-end
+task variable_stimulus(input [2:0] sppr_i1,spr_i1, input cpol_i1,cpha_i1);
+	begin
+		sppr_i=sppr_i1;
+		spr_i=spr_i1;
+		cpol_i=cpol_i1;
+		cpha_i=cpha_i1;
+	end
 endtask
+
 
 initial
-begin
-    initialize;
-    reset;
+fork
+	resetn();
+	fixed_stimulus(1'b0,1'b0,2'b00);
+	variable_stimulus(3'b000,3'b010,0,1);
+	#500
+	variable_stimulus(3'b000,3'b010,0,0);
+join
 
-    stimulus(3'b001,3'b001,1'b1,1'b1);
-    #200;
-
-    stimulus(3'b001,3'b001,1'b0,1'b1);
-    #200;
-
-    stimulus(3'b001,3'b001,1'b1,1'b0);
-    #200;
-
-    stimulus(3'b001,3'b001,1'b0,1'b0);
-    #200;
-end
-
-initial
-begin
-    $monitor("baudrate_divisor=%d", baud_rate_divisor);
-   
-end
 endmodule
+
+
